ea0770cd482343a54ed97ee562022b0125ff2342
Vuong Vu <vuongvu061101@gmail.com>
2026-05-28 03:44:34 +0700

    refactor

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
+if (!window.Shopify) return;
+
 import { bootstrapApp } from './core/bootstrap';
 
 (() => {
-  if (!window.Shopify) return;
-
   const app = bootstrapApp();
   window.addEventListener('bss_b2b:module:loaded', app, { once: true });
 })();
