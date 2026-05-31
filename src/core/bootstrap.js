0aca1da2ea70ed4f1e0a974555ea6b44637ea29c
Vuong Vu <vuongvu061101@gmail.com>
2026-05-31 23:47:16 +0700

    refactor

--- src/core/bootstrap.js
+++ src/core/bootstrap.js
@@ -6,6 +6,7 @@ import { initEventTracker } from "./event-tracker";
 
 export function bootstrapApp() {
   window.bssB2BHooks = window.bssB2BHooks ?? { actions: {}, filters: {} };
+  console.log('Bootstrapping BSS B2B Toolkit...', structuredClone(window.bssB2BHooks));
   window.BSS_B2B = window.BSS_B2B ?? {};
   BSS_B2B.addAction = (tag, callback) => (bssB2BHooks.actions[tag] = callback);
   BSS_B2B.addFilter = (tag, callback) => (bssB2BHooks.filters[tag] = callback);
@@ -27,76 +28,71 @@ export function bootstrapApp() {
     // load config
     // shopStorage.set('configs', { isHighlighted: true });
   })();
-  BSS_B2B.addFilter('custom:loadCurrency', shouldLoadCurrency => {
-    if (customSelectors.hasData()) customSelectors.init();
-    
-    const shopStorage = createStorage(Shopify.theme.schema_name);
-  
-    BSS_B2B.support = {
-      collection: {},
-      search: {},
-      forms: {},
-      cart: {},
-      utils: {},
-      shopStorage,
-      configs: {
-        values: publicConfig,
-        save() {
-          return config.save();
-        },
-      },
-      customSelectors,
-    };
-  
-    Object.assign(BSS_B2B.support.utils, {
-      // initCustomSelectors,
-      // processProductCards,
-      // processCart,
-      // processForm,
-      // addRunnableFn,
-      // registerFn,
-      // getCustomFnNames,
-      // getCustomFn,
-      // removeCustomFn,
-      // getShopifyProductLink,
-      // getShopifyVariantLink,
-      // getAvailableRules,
-      // getAppliedRules,
-      // generateCode,
-      // upsertStyleTag,
-      highlightSearch,
-      highlightCollection,
-      highlightForms,
-      highlightCart,
-      unhighlightSearch,
-      unhighlightCollection,
-      unhighlightForms,
-      unhighlightCart,
-    });
+if (customSelectors.hasData()) customSelectors.init();
 
-    console.log(
-      '%c 🛠️ B2B Toolkit v1.0.0 %c by LV ✨',
-      `
-        background: linear-gradient(90deg, #667eea 0%, #764ba2 100%);
-        color: white;
-        padding: 8px 12px;
-        border-radius: 10px 0 0 10px;
-        font-size: 14px;
-        font-weight: 700;
-      `,
-      `
-        background: #111827;
-        color: #93c5fd;
-        padding: 8px 8px;
-        border-radius: 0 10px 10px 0;
-        font-size: 13px;
-        font-weight: 600;
-      `
-    );
-    return shouldLoadCurrency;
-  });
+const shopStorage = createStorage(Shopify.theme.schema_name);
 
+BSS_B2B.support = {
+  collection: {},
+  search: {},
+  forms: {},
+  cart: {},
+  utils: {},
+  shopStorage,
+  configs: {
+    values: publicConfig,
+    save() {
+      return config.save();
+    },
+  },
+  customSelectors,
+};
 
+Object.assign(BSS_B2B.support.utils, {
+  // initCustomSelectors,
+  // processProductCards,
+  // processCart,
+  // processForm,
+  // addRunnableFn,
+  // registerFn,
+  // getCustomFnNames,
+  // getCustomFn,
+  // removeCustomFn,
+  // getShopifyProductLink,
+  // getShopifyVariantLink,
+  // getAvailableRules,
+  // getAppliedRules,
+  // generateCode,
+  // upsertStyleTag,
+  highlightSearch,
+  highlightCollection,
+  highlightForms,
+  highlightCart,
+  unhighlightSearch,
+  unhighlightCollection,
+  unhighlightForms,
+  unhighlightCart,
+});
+
+console.log(
+  '%c 🛠️ B2B Toolkit v1.0.0 %c by LV ✨',
+  `
+    background: linear-gradient(90deg, #667eea 0%, #764ba2 100%);
+    color: white;
+    padding: 8px 12px;
+    border-radius: 10px 0 0 10px;
+    font-size: 14px;
+    font-weight: 700;
+  `,
+  `
+    background: #111827;
+    color: #93c5fd;
+    padding: 8px 8px;
+    border-radius: 0 10px 10px 0;
+    font-size: 13px;
+    font-weight: 600;
+  `
+);
 
   return () => {
       initEventTracker();
--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import { bootstrapApp } from './core/bootstrap';
-
-(() => {
+async function onB2bReady() {
+  if (!window.Shopify) return;
+  const { bootstrapApp } = await import('./core/bootstrap');
   const app = bootstrapApp();
   window.addEventListener('bss_b2b:module:loaded', app, { once: true });
-})();
+}
+
+if (window.bssB2BHooks !== undefined) {
+  onB2bReady();
+} else {
+  Object.defineProperty(window, 'bssB2BHooks', {
+    configurable: true,
+
+    set(value) {
+      Object.defineProperty(window, 'bssB2BHooks', {
+        value,
+        writable: true,
+        configurable: true,
+        enumerable: true,
+      });
+
+      setTimeout(onB2bReady);
+    },
+
+    get() {
+      return undefined;
+    },
+  });
+}
