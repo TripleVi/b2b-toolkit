fdddef8f8e8afe8902bcb96592a74825c5cb09b5
TripleVi <106103028+TripleVi@users.noreply.github.com>
2026-06-07 14:59:31 +0000

    Deploying to gh-pages from @ TripleVi/b2b-toolkit@d2188abe5d914f57584c99ea177962c1102ada63 🚀

--- b2b-toolkit.user.js
+++ b2b-toolkit.user.js
@@ -1182,7 +1182,7 @@
     font-weight: 600;
   `);
 		const storage = createStorage();
-		BSS_B2B.logger.log(`Dev mode ${storage.get("devMode") ? "enabled" : "disabled"}`);
+		console.log(`%c[bss.b2b] Dev mode ${storage.get("devMode") ? "enabled" : "disabled"}`, "color: pink; font-weight: 600; font-style: italic;");
 		return () => {
 			initEventTracker();
 			handleSearch();
